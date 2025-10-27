73f5f04cdb8b634223a4363e87f32996cdb2c18c
p1nero <1070402601@qq.com>
2025-10-28 01:26:29 +0800

    更新对话lib

--- src/main/java/com/p1nero/tudigong/entity/TudiGongEntity.java
+++ src/main/java/com/p1nero/tudigong/entity/TudiGongEntity.java
@@ -1,6 +1,6 @@
 package com.p1nero.tudigong.entity;
 
-import com.p1nero.dialog_lib.api.IEntityNpc;
+import com.p1nero.dialog_lib.api.custom.IEntityNpc;
 import com.p1nero.dialog_lib.api.goal.LookAtConservingPlayerGoal;
 import com.p1nero.dialog_lib.client.screen.DialogueScreenBuilder;
 import com.p1nero.dialog_lib.network.DialoguePacketRelay;
